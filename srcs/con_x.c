f182a1d5b93056eaaeb32bf3236b0c700a48ae6b
Ninjapiraatti <tuoppil@gmail.com>
2020-04-10 12:30:35 +0300

    Working on u and x

--- srcs/con_x.c
+++ srcs/con_x.c
@@ -6,7 +6,7 @@
 /*   By: tlouekar <tlouekar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/11 13:36:41 by tlouekar          #+#    #+#             */
-/*   Updated: 2020/01/27 12:35:08 by tlouekar         ###   ########.fr       */
+/*   Updated: 2020/04/10 12:28:40 by tlouekar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void			con_x(t_printf *data, va_list args)
 {
-	ft_putchar(va_arg(args, char));
+	char		*str;
+
+	if (data->star == 1)
+		data->fieldwidth = va_arg(args, int);
+	data->lli = (long long)va_arg(args, int);
+	str = helper_itoa_base((unsigned long long)data->lli, 16, data);
+	data->len = ft_strlen(str) + 2;
+	if (data->minus == 0)
+		helper_print_padding(data);
+	ft_putstr("0x");
+	ft_putstr(str);
 }
--- srcs/output.c
+++ srcs/output.c
@@ -6,7 +6,7 @@
 /*   By: tlouekar <tlouekar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 12:21:01 by tlouekar          #+#    #+#             */
-/*   Updated: 2020/04/10 11:39:10 by tlouekar         ###   ########.fr       */
+/*   Updated: 2020/04/10 12:26:01 by tlouekar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ void		print_con_spec(t_printf *data, va_list args)
 		con_o(data, args);
 	if (data->u == 1)
 		con_u(data, args);
+	if (data->x == 1)
+		con_x(data, args);
 	if (data->f == 1 || data->lf == 1)
 		con_f(data, args);
 }
