dde47efb833c67661ec4240402b56fbd9b42c801
Ninjapiraatti <tuoppil@gmail.com>
2020-05-03 11:08:43 +0300

    Only two errors basically

--- srcs/helpers.c
+++ srcs/helpers.c
@@ -6,7 +6,7 @@
 /*   By: tlouekar <tlouekar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/11 13:37:41 by tlouekar          #+#    #+#             */
-/*   Updated: 2020/05/02 17:50:36 by tlouekar         ###   ########.fr       */
+/*   Updated: 2020/05/03 10:25:19 by tlouekar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,15 @@ void		helper_print_padding(t_printf *data)
 	i = data->fieldwidth - data->len;
 	if ((data->o != 1 && data->x != 1 && data->X != 1 && data->u != 1) && 
 	((data->plus == 1) || 
-	(data->lli < 0 && data->dot == 1) ||
-	(data->lli < 0 && data->fieldwidth > 0) ||
-	(data->flo < 0 && data->dot == 1) ||
-	(data->minus == 1 && data->flo < 0)))
+	//(data->lli < 0 && data->dot == 1) ||
+	//(data->lli < 0 && data->fieldwidth > 0) ||
+	//(data->flo < 0 && data->dot == 1) ||
+	//(data->minus == 1 && data->flo < 0)))
+	(data->fieldwidth > data->len && data->s == 0) ||
+	(data->lli < 0)))
 		i--;
-	if ((data->i == 1 || data->d == 1) && (data->prc == 0 && data->dot == 1 && data->lli == 0 && data->len == 1))
-		i++;
+	//if ((data->i == 1 || data->d == 1) && (data->prc == 0 && data->dot == 1 && data->lli == 0 && data->len == 1))
+	//	i++;
 	if ((data->prc > data->len) && data->s != 1)
 		i -= data->prc - data->len;
 	if (data->minus == 0 && data->fieldwidth > 0 && data->zero == 1 && data->dot == 0 && data->loose == 0)
@@ -53,16 +55,56 @@ void		helper_print_padding(t_printf *data)
 	}
 }
 
+/*
+
+Test 348 (i_width_pos_fits_lj_sp) : FAILED.
+    First line of code: {return test("% -7i", 33);}
+      expected output : " 33    "
+      your output     : "33     "
+
+Test 370 (i_zpsp_zero_fits) : FAILED.
+    First line of code: {return test("% 03i", 0);}
+      expected output : " 00"
+      your output     : "000"
+
+Test 388 (i_prec_width_nofit_fit_pos_lj_sp) : FAILED.
+    First line of code: {return test("% -8.3i", 8375);}
+      expected output : " 8375   "
+      your output     : "8375    "
+
+Test 424 (i_prec_width_nf_pos_ljzpsp_ignoreflag) : FAILED.
+    First line of code: {return test("% 0-8.3i", 8375);}
+      expected output : " 8375   "
+      your output     : "8375    "
+
+Test 676 (d_width_pos_fits_lj_sp) : FAILED.
+    First line of code: {return test("% -7d", 33);}
+      expected output : " 33    "
+      your output     : "33     "
+
+Test 698 (d_zpsp_zero_fits) : FAILED.
+    First line of code: {return test("% 03d", 0);}
+      expected output : " 00"
+      your output     : "000"
+	
+Test 696 (d_zpsp_pos_fits) : FAILED.
+    First line of code: {return test("% 05d", 43);}
+      expected output : " 0043"
+      your output     : "00043"
+
+*/
 void		helper_spaces(t_printf *data)
 {
 	if (data->space == 1)
 	{
 		if (data->plus == 0 && data->lli >= 0 && data->flo >= 0)
 		{
+			/*
 			if (data->fieldwidth <= data->len)
 				ft_putchar(' ');
-			//else if (data->dot == 1)
-			//	ft_putchar(' ');
+			else if (data->dot == 1)
+			*/
+			ft_putchar(' ');
 		}
 	}
 }
