ac1dd625c4b554fb4cb472b056213b8ce495b164
Ninjapiraatti <tuoppil@gmail.com>
2020-06-08 11:43:05 +0300

    Fixed leaks

--- srcs/con_s.c
+++ srcs/con_s.c
@@ -6,7 +6,7 @@
 /*   By: tlouekar <tlouekar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/11 13:36:41 by tlouekar          #+#    #+#             */
-/*   Updated: 2020/06/07 17:58:04 by tlouekar         ###   ########.fr       */
+/*   Updated: 2020/06/08 11:23:20 by tlouekar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,26 @@
 void			con_s(t_printf *data, va_list args)
 {
 	char		*str;
+	char		*tmp;
 
 	if (data->space == 1)
 		ft_putchar(' ');
 	if(!(str = va_arg(args, char *)))
-		str = ft_strdup("(null)");
-	if (data->dot > 0)
-		str = ft_strsub(str, 0, data->len);
-	data->len = ft_strlen(str);
+	{
+		if (data->dot == 0)
+			tmp = ft_strdup("(null)");
+		else
+			tmp = ft_strsub("(null)", 0, data->len);
+	}
+	else {
+		if (data->dot == 0)
+			tmp = ft_strdup(str);
+		else
+			tmp = ft_strsub(str, 0, data->len);
+	}
+	data->len = ft_strlen(tmp);
 	if (data->minus == 0)
 		helper_print_padding(data);
-	ft_putstr(str);
+	ft_putstr(tmp);
+	free(tmp);
 }
