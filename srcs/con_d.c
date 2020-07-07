266f9a4f3bb51c665b9c294f9b31c738cfec284a
Tuomas Louekari <tlouekar@c1r5p9.hive.fi>
2020-07-07 16:00:20 +0300

    Fighting with the star. Moulitest failing spectacularly now for some reason

--- srcs/con_d.c
+++ srcs/con_d.c
@@ -6,7 +6,7 @@
 /*   By: tlouekar <tlouekar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/11 13:36:41 by tlouekar          #+#    #+#             */
-/*   Updated: 2020/07/07 14:20:54 by tlouekar         ###   ########.fr       */
+/*   Updated: 2020/07/07 15:58:15 by tlouekar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,17 @@ void			con_d(t_printf *data, va_list args)
 	char		*str;
 
 	if (data->starlen == 1)
-		data->fieldwidth = va_arg(args, int);
-	else if (data->starprc == 1)
-		data->prc = va_arg(args, int);
+		data->fieldwidth = va_arg(args, uintmax_t);
+		if (data->fieldwidth < 0)
+			data->fieldwidth *= -1;
+	if (data->starprc == 1)
+	{
+		data->prc = va_arg(args, uintmax_t);
+		if (data->prc < 0)
+			;
+		else
+			data->len = data->prc;
+	}
 	handle_length(data, args);
 	str = helper_itoa_base(data->val, 10, data);
 	data->len = ft_strlen(str);
--- srcs/con_s.c
+++ srcs/con_s.c
@@ -6,7 +6,7 @@
 /*   By: tlouekar <tlouekar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/11 13:36:41 by tlouekar          #+#    #+#             */
-/*   Updated: 2020/07/07 14:11:04 by tlouekar         ###   ########.fr       */
+/*   Updated: 2020/07/07 15:50:58 by tlouekar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,25 @@ void			con_s(t_printf *data, va_list args)
 {
 	char		*str;
 	char		*tmp;
+	int			temp;
 
+	temp = 0;
+	if (data->starlen == 1)
+	{
+		temp = va_arg(args, uintmax_t);
+		if (temp < 0)
+			temp *= -1;
+		data->fieldwidth = temp;
+	}
+	temp = 0;
+	if (data->starprc == 1)
+	{
+		temp = va_arg(args, uintmax_t);
+		if (temp < 0)
+			;
+		else
+			data->len = temp;
+	}
 	if (data->spc == 1)
 		ft_putchar(' ');
 	if (!(str = va_arg(args, char *)))
